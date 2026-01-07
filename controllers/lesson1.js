44dbf47da3f50a3106a702646dbbe9d3ae85c1ce
tarydenise <tarydenise@gmail.com>
2026-01-06 17:53:45 -0800

    Adding additional routes

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -6,7 +6,22 @@ const lisiateRoute = (req, res) => {
   res.send("Lisiate Ata");
 };
 
+const kristynRoute = (req, res) => {
+  res.send("Kristyn Tupouata");
+};
+
+const markRoute = (req, res) => {
+  res.send("Mark Tupouata");
+};
+
+const richieRoute = (req, res) => {
+  res.send("Richie Tupouata");
+};
+
 module.exports = {
   taryRoute,
   lisiateRoute,
+  kristynRoute,
+  markRoute,
+  richieRoute,
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,9 @@ const lesson1Controller = require("../controllers/lesson1");
 
 routes.get("/", lesson1Controller.taryRoute);
 routes.get("/lisiate", lesson1Controller.lisiateRoute);
+routes.get("/kristyn", lesson1Controller.kristynRoute);
+routes.get("/mark", lesson1Controller.markRoute);
+routes.get("/richie", lesson1Controller.richieRoute);
+
 
 module.exports = routes;
