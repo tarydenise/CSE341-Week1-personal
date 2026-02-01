20766d439cf7e74931596fd8debe28d2c9bd26a0
tarydenise <tarydenise@gmail.com>
2026-01-31 18:33:17 -0800

    Fixing CORS issue

--- server.js
+++ server.js
@@ -7,29 +7,35 @@ const app = express();
 
 const { swaggerUi, swaggerSpec } = require('./swagger');
 
-app
-    .use(bodyParser.json())
-    .use((req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"           
-        );
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
 
-        if (req.method === "OPTIONS") {
-            return res.sendStatus(200);
-        }
-        
-        next();
-    })
-    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-    .use('/contacts', require('./routes/contactsRoute'))
-    .use("/", require("./routes"));
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/contacts', require('./routes/contactsRoute'));
+app.use("/", require("./routes"));
 
 mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.log(err);
-    } else {
-        app.listen(port);
-        console.log(`Connected to DB and listening on port ${port}`);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on port ${port}`);
+  }
 });
